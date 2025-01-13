fb73f3c94704f0778ada4a7f93662bf3bf42e6ab
Kathrin Weber <s2310456029@fhooe.at>
2025-01-13 13:58:03 +0100

    Code vor slider

--- packages/kwm24/Resources/Public/JavaScript/Dist/scripts.js
+++ packages/kwm24/Resources/Public/JavaScript/Dist/scripts.js
@@ -1,6 +1,36 @@
-/*!
- * kwm24 v1.0.0 (https://fh-ooe.at/campus-hagenberg)
- * Copyright 2017-2024 Kathrin Weber
- * Licensed under the GPL-2.0-or-later license
- */
-console.log("WE LOVE TYPO3");
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 10000,
+    },
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+
+    // Coverflow Effect Parameters
+    effect: 'coverflow', // Aktiviert den Coverflow-Effekt
+    coverflowEffect: {
+        depth: 100, // Tiefe des Effekts (Z-Achse)
+        modifier: 1, // Effektmodifikator (Steuert die Intensität des Effekts)
+        rotate: 50, // Drehung der Slides in Grad
+        scale: 1, // Skalierung der Slides
+        slideShadows: true, // Aktiviert die Schatten der Slides
+        stretch: 0, // Abstand zwischen den Slides
+    },
+
+    // Wenn du eine Pagination benötigst
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Falls du eine Scrollbar benötigst
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
